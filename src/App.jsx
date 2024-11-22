6f0953da93e36ea168e02bc6ccc0fa1c7949d16d
thiarape21 <80516444+thiarape21@users.noreply.github.com>
2024-11-22 13:19:49 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
-
 import Gallery from './Gallery';
 import Countdown from './Countdown';
 
 const App = () => {
   const audioRef = useRef(null); 
   const [isPlaying, setIsPlaying] = useState(false); 
+
   const openLocation = () => {
     window.open("https://goo.su/83FW5VN", "_blank");
   };
@@ -22,24 +22,26 @@ const App = () => {
 
   return (
     <div className="invitation-container">
-
       <audio ref={audioRef} loop>
         <source src="/musica/musica.mp3" type="audio/mp3" />
         Tu navegador no soporta el elemento de audio.
       </audio>
 
-      <div className="content"><br /><br />
+      <div className="content">
+        <br /><br />
         <button 
-        onClick={handlePlayPauseMusic} 
-        className="music-button"
+          onClick={handlePlayPauseMusic} 
+          className="music-button"
         >
           {isPlaying ? "Pausar Música" : "Reproducir Música"}
-          </button>
-        <div className="header"><br />
+        </button>
+        <div className="header">
+          <br />
           <h1 className="title">Estás Invitado</h1>
           <p className="subtitle">a celebrar los 70 años de </p><br />
           <h1 className="name">Yolanda</h1><br /><br />
         </div>
+
         <div className="details">
           <Gallery />
           <h1>_________</h1>
@@ -52,36 +54,40 @@ const App = () => {
             📅 <strong>Fecha:</strong> 12 de Febrero <br /> <br />
             🕒 <strong>Hora:</strong> 3:30 PM<br /><br />
             🎩 <strong>Vestimenta:</strong> Formal<br /><br />
-            📍 <strong>Ubicación:</strong>{" "}<br /><br />
+            📍 <strong>Ubicación:</strong> <br /><br />
             <button onClick={openLocation}>Ver Ubicación</button>
           </p>
+
           <h1>_________</h1>
           <h2>Presentes</h2>
           🎁🍷
           <p>
-            <strong>Tu compañia siempre es lo más importante</strong> pero agradecería
+            <strong>Tu compañía siempre es lo más importante</strong> pero agradecería
             mucho si en este día tan especial me obsequias alguna de estas botellas de vino:<br />
           </p>
+
           <div className="wines">
-            <div className="wine"><br />
-              <img src="/images/vino1.jpg" alt="Vino 1" />
-              <p>Vino 1</p>
+            <div className="wine">
+              <br />
+              <img src="/images/vino1.png" alt="Old Parr" />
+              <p>Old Parr</p>
             </div>
-            <div className="wine"><br />
-              <img src="/images/vino2.jpg" alt="Vino 2" />
-              <p>Vino 2</p>
+            <div className="wine">
+              <br />
+              <img src="/images/vino2.png" alt=" Black Label" />
+              <p>Black Label</p>
+            </div>
+            <div className="wine">
+              <br />
+              <img src="/images/vino3.png" alt="Marqués de Cáceres" />
+              <p>Marqués de Cáceres</p>
             </div>
           </div>
+
           <h1>_________</h1>
           <h2>Confirma tu asistencia</h2>
           <p>Confirma tu asistencia al (506) 7102-7304</p>
           <p>Thiara Espinoza</p>
-
-  
-        </div>
-      
-        <div className="footer">
-        
         </div>
       </div>
     </div>
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,3 @@
-// Gallery.jsx
 import React from 'react';
 import foto1 from './images/foto1.jpeg';
 import foto2 from './images/foto2.jpeg';
@@ -14,14 +13,22 @@ import foto14 from './images/foto14.jpeg';
 import foto15 from './images/foto15.jpeg';
 
 const Gallery = () => {
-  const photos = [foto1, foto2, foto3, foto4, foto5, foto6, foto10,
-    foto11, foto12, foto15, foto13, foto14];
+  const photos = [foto1, foto3, foto4, foto5, foto6, foto10, foto11, foto12, foto15, foto13, foto14];
 
   return (
     <div className="gallery">
+      {/* Imagen de portada */}
+      <div className="cover-photo">
+        <img src={foto2} alt="Foto de portada" />
+      </div>
+
+      {/* Galería de fotos */}
       <div className="photos">
         {photos.map((photo, index) => (
-          <div key={index} className={`photo-container ${index === 12 || index === 13 ? 'horizontal' : ''}`}>
+          <div
+            key={index}
+            className={`photo-container ${index === 9 || index === 10 ? 'horizontal' : ''}`}
+          >
             <img src={photo} alt={`Foto ${index + 1}`} />
           </div>
         ))}
